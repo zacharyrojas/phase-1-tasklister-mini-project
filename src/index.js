e6e78e31808b6878f985e4d75dadab1e261b8a8b
Zachary Rojas <ztr2194@gmail.com>
2023-04-06 14:08:21 -0500

    tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (x) => {
+      x.preventDefault()
+      addAll(x.target.new_task_description.value)
+      form.reset()
+   })})
+
+   function addAll(everything) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', toDelete)
+    btn.textContent = 'x'
+    p.textContent = `${everything}  `
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
+   }
+
+   function toDelete(e) {
+    e.target.parentNode.remove()
+   }
